54e84a704563e8df3e14dcd3356d64e3f0d1e233
Nafiuls <snafiul700@gmail.com>
2024-10-03 01:20:31 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const path = require('path');
+const dotenv = require('dotenv');
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+dotenv.config();
+
+app.get('/', (req, res) => {
+  res.send('the server is running');
+});
+
+app.listen(5000, () => {
+  console.log('Server running on port 5000');
+});
